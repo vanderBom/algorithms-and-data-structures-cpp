e7799c9702cb4ba2ec10480335fd5bc0faade256
van der Bom <isaacvanderbom4149@gmail.com>
2026-07-07 01:19:02 -0600

    feat: constructor/destructor

--- include/ds/dynamic_array.hpp
+++ include/ds/dynamic_array.hpp
@@ -8,6 +8,23 @@ private:
     T* data;
     unsigned int arrSize;
     unsigned int cap;
+
+    void resize (unsigned int newcap){
+        T* newData=new T[newcap];
+
+        for (unsigned int i = 0; i < arrSize; i++){
+            newData[i] = data[i];
+        }
+        
+    }
 public:
+    DynamicArray(){
+        data = nullptr;
+        arrSize = 0;
+        cap = 0;
+    }
+    ~DynamicArray(){
+        delete[] data;
+    }
 
 }
