85d53408b6e021e0676f64fb70af7956e4272c2c
van der Bom <isaacvanderbom4149@gmail.com>
2026-07-07 15:19:57 -0600

    feat: push-back function implementation

--- include/ds/dynamic_array.hpp
+++ include/ds/dynamic_array.hpp
@@ -12,7 +12,7 @@ private:
     void resize (unsigned int newcap){
         T* newData=new T[newcap];
 
-        for (unsigned int i = 0; i < arrSize; i++){
+        for (unsigned int i = 0; i < arrSize ; i++){
             newData[i] = data[i];
         }
 
@@ -39,7 +39,13 @@ public:
     //if the array is full we have to expand it
     //or we have to insert the value at the end of the array and increment by 1 the array size
 
-    void push_back (T value){
-        
+    void push_back (T& value){
+        if (data==nullptr){
+            resize (1);
+        }else if (cap == arrSize){
+            resize (cap*2);
+        }
+        data[arrSize] = value;
+        arrSize++;
     }
 }
