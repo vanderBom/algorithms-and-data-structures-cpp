b1d428a64e1e2e6db7998d8bd334120c63b8d839
van der Bom <isaacvanderbom4149@gmail.com>
2026-08-08 16:02:39 -0600

    chore: new classfor stack

---
+++ include/ds/stack.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+template <typename T>
+class Stack {
+    private:
+        DynamicArray<T> data;
+    public:
+        
+}
