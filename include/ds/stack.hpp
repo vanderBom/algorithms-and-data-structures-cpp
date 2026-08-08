0fd50e993207184c358d07b15993484d0a46669a
van der Bom <isaacvanderbom4149@gmail.com>
2026-08-08 16:16:57 -0600

    feat: size and empty methods

--- include/ds/stack.hpp
+++ include/ds/stack.hpp
@@ -22,6 +22,12 @@ class Stack {
             if (data.size() == 0) throw std::out_of_range("Stack is empty");
             else return data.at(data.size()-1);
         }
-        
+        unsigned int size() const{
+            return data.size();
+        }
+        bool empty(){
+            if (data.size() == 0) return true;
+            else return false;
+        }
         
 };
