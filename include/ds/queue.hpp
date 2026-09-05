1547a8f4360ab53bcd78c12f92e61772b2563ad0
van der Bom <isaacvanderbom4149@gmail.com>
2026-09-05 01:47:59 -0600

    chore: Set up the todo's for queue

---
+++ include/ds/queue.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class Queue {
+    private: //Atributes for queue
+
+    public: //Methods for queue
+    //Todo: Implement constructor and destructor
+
+    //Todo: Implement tree of five rule
+
+    //Todo: Implement push method
+
+    //Todo: Implement pop method
+
+    //Todo: Implement front method
+
+    //Todo: Implement back method
+
+    //Todo: Implement size method
+
+    //Todo: Implement empty method
+
+    //Todo: Implement clear method
+}
